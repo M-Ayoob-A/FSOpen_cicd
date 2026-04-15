40e561e4fa5a486b87e1d73fb240ccde31978172
Otshal Puri <itsmeotshal@gmail.com>
2026-04-15 22:38:13 +0300

    restoring the health error code

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 const express = require('express')
 const app = express()
 
-/*app.get('/health', (req, res) => {
-  res.send('ok')
-})*/
-
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.send('ok')
 })
 
