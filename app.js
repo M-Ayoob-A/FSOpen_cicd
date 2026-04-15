042ed8e9aa4d0488140f9177f032a374c3951888
Otshal Puri <itsmeotshal@gmail.com>
2026-04-15 10:09:09 +0300

    health and version endpoint added

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1')
+})
+
 // get the port from env variable
 const PORT = process.env.PORT || 5001
 
