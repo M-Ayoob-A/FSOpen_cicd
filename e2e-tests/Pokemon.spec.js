c508035ad80e9dae89092862878b43dbda85b9b2
William-nyarash <owetywilliamnyaranga@gmail.com>
2026-05-22 11:54:49 +0300

    feat: add e2e test

---
+++ e2e-tests/Pokemon.spec.js
@@ -0,0 +1,8 @@
+// @ts-check
+import { test, expect } from '@playwright/test'
+test('front page can be opened', async ({ page }) => {
+    await page.goto('http://localhost:8080/')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible( )
+
+})
